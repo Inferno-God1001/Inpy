64ce6711001ecc8751c5ec76fc3740dd35c54998
DimenCoder <renangameslinha8@gmail.com>
2026-03-16 17:37:21 -0300

    Atualizar o inpy.py

--- inpy/inpy.py
+++ inpy/inpy.py
@@ -1,6 +1,7 @@
 import sys
 import termios
 import tty
+import string
 
 CORES = {
     "black": "30",
@@ -51,11 +52,43 @@ def _gerar_codigo(style):
     return "\033[" + ";".join(codigos) + "m"
 
 
-def input(prompt="", style="white", hide=False, hide_full=False, hide_char="*"):
-    texto = ""
-    codigo = _gerar_codigo(style)
+def _permitido(tecla, modo):
 
-    print(prompt, end="", flush=True)
+    if modo == "all":
+        return True
+
+    if modo == "numbers":
+        return tecla.isdigit()
+
+    if modo == "letters":
+        return tecla.isalpha()
+
+    if modo == "alphanumeric":
+        return tecla.isalnum()
+
+    if modo == "chars":
+        return tecla in string.punctuation
+
+    return True
+
+
+def input(
+    prompt="",
+    message_color="white",
+    color="white",
+    hide=False,
+    hide_full=False,
+    hide_char="*",
+    auto_enter_word=None,
+    accept="all"
+):
+
+    texto = ""
+
+    codigo_prompt = _gerar_codigo(message_color)
+    codigo_texto = _gerar_codigo(color)
+
+    print(codigo_prompt + prompt + RESET, end="", flush=True)
 
     while True:
         tecla = _ler_tecla()
@@ -75,14 +108,20 @@ def input(prompt="", style="white", hide=False, hide_full=False, hide_char="*"):
                     sys.stdout.flush()
             continue
 
+        if not _permitido(tecla, accept):
+            continue
+
         texto += tecla
+
+        if auto_enter_word and texto.endswith(auto_enter_word):
+            break
 
         if hide_full:
             pass
         elif hide:
             sys.stdout.write(hide_char)
         else:
-            sys.stdout.write(codigo + tecla + RESET)
+            sys.stdout.write(codigo_texto + tecla + RESET)
 
         sys.stdout.flush()
 
