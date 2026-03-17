07945d69da4ef5a86dc2cec6891e4881aa61deed
DimenCoder <renangameslinha8@gmail.com>
2026-03-17 13:57:17 -0300

    Atualizar o setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="inpy",
-    version="1.1.5",
+    version="1.1.6",
     description="Simple",
     author="Inferno-God1001",
     packages=find_packages(),
